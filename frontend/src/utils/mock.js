4a7c4849701a3fbee6b91cf20de2f8c2965fc316
PELERIN Chloé <pelerinchloe@gmail.com>
2024-11-06 23:47:12 +0100

    folders structure & add mock for users & places

---
+++ frontend/src/utils/mock.js
@@ -0,0 +1,58 @@
+export const mockUsers = [
+    {
+        "id": 1,
+        "name": "Martine Carre",
+        "username": "mcarre",
+        "email": "martine_carre@gmail.com",
+        "geo": {
+            "lat": 48.849726,
+            "lng": 2.319596
+        }
+    },
+    {
+        "id": 2,
+        "name": "Nicole Fleury",
+        "username": "nfleury1",
+        "email": "nicole_fleury@zohomail.com",
+        "geo": {
+            "lat": 48.885101,
+            "lng": 2.298035
+        }
+      },
+    {
+        "id": 3,
+        "name": "Amaryllis",
+        "username": "Kittycat3",
+        "email": "amaryllis_noel@yahoo.com",
+        "geo": {
+            "lat": 48.867143,
+            "lng": 2.29906
+        }
+      }
+
+]
+
+export const mockPlaces = [
+    {
+        "id": 1,
+        "name": "Canal Saint-Denis",
+        "description": "Jolis streets arts le long du canal",
+        "address":"Quai de l'Oise, 75019 Paris",
+        "geo": {
+            "lat": 48.890250,
+            "lng": 2.382670
+        },
+        "accessibility": true
+    },
+    {
+        "id": 2,
+        "name": "Jardin Villemin",
+        "description": "Beau petit écrin de verdure au milieu de la folie parisienne. C'est calme et on est depaysé",
+        "address":"14 Rue des Récollets, 75010 Paris",
+        "geo": {
+            "lat": 48.875021,
+            "lng": 2.361086
+        },
+        "accessibility": true
+    }
+]
