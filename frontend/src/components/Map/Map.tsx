a592b74d9bfeb1a180003ed758ac9a46c4693e39
pchloe02 <pelerinchloe@gmail.com>
2024-11-15 11:15:50 +0100

    LOOP-9 add the map at HomePage

---
+++ frontend/src/components/Map/Map.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+export const Map = () => {
+  return (
+    <MapContainer center={[48.849726, 2.319596]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
