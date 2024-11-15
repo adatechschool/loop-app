75af93beda205d7eed977802df8a3c6afb9f5de6
Eliana. <147487242+Arteinsana7@users.noreply.github.com>
2024-11-15 11:03:20 +0100

    remove comentaires

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './assets/logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; 
+import HomePage from './pages/HomePage'; 
+import ListPage from './pages/ListPage';
+import AddPage from './pages/AddPage';
+import SearchPage from './pages/SearchPage';
+import ProfilePage from './pages/ProfilePage';
+import Navbar from './components/NavBar';
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar /> {/* Your navigation bar */}
+      <Routes>
+        <Route path="/home" element={<HomePage />} /> {/* Route for Home Page */}
+        <Route path="/map" element={<ListPage />} /> {/* Route for Map Page */}
+        <Route path="/add" element={<AddPage />} /> {/* Route for Add Page */}
+        <Route path="/search" element={<SearchPage />} /> {/* Route for Search Page */}
+        <Route path="/profile" element={<ProfilePage />} /> {/* Route for Profile Page */}
+        {/* You can add a default route or fallback */}
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,4 @@
+// src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -5,24 +6,25 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      {/* Wrap the App with Router to enable routing */}
+      <Router>
+        <App />
+      </Router>
     </ChakraProvider>
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
+// Service worker registration (for PWA functionality)
 serviceWorkerRegistration.register();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Web Vitals (for performance monitoring)
 reportWebVitals();
