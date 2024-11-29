d88f5dbfabf4ca5fb6b5ec2fe32dbc06764a22a8
Eliana <147487242+Arteinsana7@users.noreply.github.com>
2024-11-29 16:57:51 +0100

    avatar

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,7 @@
 import HomePage from "./Homepage/HomePage";
 import AddPage from "./AddPage/AddPage";
 import ListPage from "./ListPage/ListPage";
-import ProfilePage from "./ProfilePage";
+import ProfilePage from "./ProfilePage/ProfilePage";
 import SearchPage from "./SearchPage/SearchPage";
 
 export { HomePage, ListPage, ProfilePage, SearchPage, AddPage };
