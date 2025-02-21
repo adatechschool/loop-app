964a6a30973af204da08338d8b620a3ce84fc174
pchloe02 <pelerinchloe@gmail.com>
2025-02-21 10:38:13 +0100

    display place details location on map

--- frontend/src/pages/DetailPage/DetailPage.tsx
+++ frontend/src/pages/DetailPage/DetailPage.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Box, Text, VStack, Flex, Button } from '@chakra-ui/react';
-import { useParams, useNavigate } from 'react-router-dom';
-import ImageCarousel from '../../components/Card/ImageCarrousel';
-import LikeButton from '../../components/Card/LikeButton'; 
+import React from "react";
+import { Box, Text, VStack, Flex, Button } from "@chakra-ui/react";
+import { useParams, useNavigate } from "react-router-dom";
+import ImageCarousel from "../../components/Card/ImageCarrousel";
+import LikeButton from "../../components/Card/LikeButton";
 
 interface DetailPageProps {
   data: {
@@ -15,16 +15,21 @@ interface DetailPageProps {
     };
     image: string[];
   }[];
-  favorites: string[]; 
+  favorites: string[];
   onAddToFavorites: (name: string) => void;
 }
 
-const DetailPage: React.FC<DetailPageProps> = ({ data, favorites, onAddToFavorites }) => {
-  const { name } = useParams<{ name: string }>(); 
+const DetailPage: React.FC<DetailPageProps> = ({
+  data,
+  favorites,
+  onAddToFavorites,
+}) => {
+  const { name } = useParams<{ name: string }>();
   const navigate = useNavigate();
 
-  
-  const item = data.find((item) => item.name.toLowerCase() === name?.toLowerCase());
+  const item = data.find(
+    (item) => item.name.toLowerCase() === name?.toLowerCase()
+  );
 
   if (!item) {
     return (
@@ -39,40 +44,40 @@ const DetailPage: React.FC<DetailPageProps> = ({ data, favorites, onAddToFavorit
     );
   }
 
-  
   const isFavorite = favorites.includes(item.name);
 
   return (
     <Box maxW="lg" mx="auto" p="4">
-
       <ImageCarousel images={item.image} title={item.name} />
 
-
       <Flex justifyContent="space-between" my="4">
-       
-        <LikeButton title={item.name} onAddToFavorites={onAddToFavorites} isFavorite={isFavorite} />
+        <LikeButton
+          title={item.name}
+          onAddToFavorites={onAddToFavorites}
+          isFavorite={isFavorite}
+        />
 
-        <Button colorScheme="teal" onClick={() => navigate("/list")}>
+        <Button
+          colorScheme="teal"
+          onClick={() =>
+            (window.location.href = `/?lat=${item.geo.lat}&lng=${item.geo.lng}`)
+          }
+        >
           View on Map
         </Button>
       </Flex>
 
-     
       <Text fontWeight="bold" fontSize="3xl" mb="4" mt="6" textAlign="left">
         {item.name}
       </Text>
 
-    
       <VStack align="flex-start" spacing="4" mt="6">
-   
         <Text fontSize="lg">{item.description}</Text>
 
-      
         <Text fontSize="md" fontWeight="bold">
           Address:
         </Text>
         <Text>{item.address}</Text>
-
 
         <Text fontSize="md" fontWeight="bold">
           Coordinates:
