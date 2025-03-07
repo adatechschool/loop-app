f368a5f3ba01735c8fbdcec75f889c09b460b4cc
Eliana. <e.arteinsana@gmail.com>
2025-03-07 15:00:47 +0100

    small modifs

--- frontend/src/pages/LogInPage/SignUpForm.tsx
+++ frontend/src/pages/LogInPage/SignUpForm.tsx
@@ -15,7 +15,7 @@ const SignupForm: React.FC = () => {
   };
 
   return (
-    <Container p={0} minH="100vh" display="flex" alignItems="center" justifyContent="center">
+    <Container p={4} minH="100vh" display="flex" alignItems="center" justifyContent="center">
       <Box w="full" maxW="md" p={6} borderWidth={1} borderRadius="md">
         <Heading textAlign="center" mb={6}>Créer un compte</Heading>
         <Stack spacing={4} as="form" onSubmit={handleSubmit}>
